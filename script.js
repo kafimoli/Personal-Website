890a712b1a7e6afc127dfe4a4c18bc433fca136e
kafimoli <khylonloresca@gmail.com>
2026-02-15 17:36:32 +0800

    added floating hearts and petals

--- script.js
+++ script.js
@@ -57,3 +57,63 @@ backBtn.addEventListener("click", () => {
     if (prevIndex < 0) return;
     showText(prevIndex);
 });
+
+const heartsContainer = document.querySelector(".hearts-container");
+
+function createHeart(){
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤";
+
+    // random horizontal position
+    heart.style.left = Math.random() * 100 + "vw";
+
+    // random size
+    heart.style.fontSize = (Math.random() * 20 + 10) + "px";
+
+    // random animation duration (slow + dreamy)
+    heart.style.animationDuration = (Math.random() * 5 + 5) + "s";
+
+    heartsContainer.appendChild(heart);
+
+    // remove heart after animation
+    setTimeout(() => {
+        heart.remove();
+    }, 10000);
+}
+
+// create heart every 800ms
+setInterval(createHeart, 1500);
+
+/* ===========================
+   GENERATE FALLING PETALS
+=========================== */
+
+const petalsContainer = document.querySelector(".petals-container");
+
+function createPetal(){
+    const petal = document.createElement("div");
+    petal.classList.add("petal");
+
+    // using flower emoji for simplicity
+    petal.innerHTML = "🌸";
+
+    // random horizontal position
+    petal.style.left = Math.random() * 100 + "vw";
+
+    // random size
+    petal.style.fontSize = (Math.random() * 15 + 15) + "px";
+
+    // random fall duration (slow and romantic)
+    petal.style.animationDuration = (Math.random() * 5 + 6) + "s";
+
+    petalsContainer.appendChild(petal);
+
+    // remove after animation
+    setTimeout(() => {
+        petal.remove();
+    }, 11000);
+}
+
+// create petal every 900ms
+setInterval(createPetal, 1500);
